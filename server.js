1ee223ca55d95ece352c70e813ec802673c29c4b
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-04-27 17:29:31 +0530

    added get function to fetch headers

--- server.js
+++ server.js
@@ -24,6 +24,18 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// here we will fetch all request headers required for application
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    "ipaddress" : req.id,
+    "language"  : req.headers['accept-language'],
+    "software"  : req.headers['user-agent'] 
+  });
+});
+
+
+
+
 
 
 // listen for requests :)
